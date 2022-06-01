66f01865163c4300694c1026d94caa81aaa5977d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-06-01 00:32:57 +0000

    test: init

---
+++ __tests__/cache.spec.ts
@@ -0,0 +1,19 @@
+import { CacheClient } from '@src/cache'
+
+const server = 'ws://cache:8080'
+
+describe('CacheClient', () => {
+  test('set, get', async () => {
+    const client = await CacheClient.create({ server })
+
+    try {
+      await client.set('namespace', 'key', 'value', Infinity, 0)
+      const result = await client.get('namespace', 'key')
+
+      expect(result).toBe('value')
+    } finally {
+      await client.clear('namespace')
+      await client.close()
+    }
+  })
+})
